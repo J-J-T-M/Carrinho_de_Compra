61b5818eeeb81875292663dd6816715cd3cc9aef
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-03 18:34:36 -0400

    create migration contacts

---
+++ database/migrations/2023_02_03_221828_create_contacts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('cpf');
+            $table->bigInteger('cep');
+            $table->bigInteger('phone');
+            $table->bigInteger('whatsapp');
+
+            /*
+                Relacionamento com usuario
+            */
+            // se deletar o user que cadastrou algum produto os produtos serão deletados
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
