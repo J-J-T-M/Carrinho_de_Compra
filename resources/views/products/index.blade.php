2c3ce7e467f4bff712af10c08901fe344fae3fd5
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-07 08:33:34 -0400

    working in the product index

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layouts')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-sm-12 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $product->url_image }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ Str::limit( $product->name, 25) }}</h5>
+                            <p class="card-text">{{ Str::limit($product->description, 40) }}</p>
+                            <a href="#" class="btn btn-primary"><i class="fa-solid fa-plus"></i></a>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+            @endforeach
+        </div>
+        <div class="d-flex">
+            {{ $products->links() }}
+        </div>
+    </div>
+
+@endsection
