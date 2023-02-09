5a5c42e19a24542fc6ce587a46585e877a2a67da
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-09 08:47:12 -0400

    add route categories::resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', 'products/');
 
 Route::resource('products', ProductController::class);
+
+Route::resource('categories', CategoryController::class);
