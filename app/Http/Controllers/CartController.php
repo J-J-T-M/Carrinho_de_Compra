74913cb3f72b482e2927873ae43303287579b19a
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-09 12:18:06 -0400

    construindo os method de addCart e cartList

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,6 +9,20 @@ class CartController extends Controller
     public function cartList()
     {
         $items = \Cart::getContent();
-        dd($items);
+        return view('cart.cart',['items' => $items]);
+    }
+
+    public function addCart(Request $request)
+    {
+        \Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->url_img
+            )
+        ]);
+        return redirect('cart');
     }
 }
