a421795bf0d6dff586d04d29f652f2ead318fa33
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-09 09:02:29 -0400

    add price of product and link of category

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,13 +10,14 @@
         </div>
         <div class="col-sm-12 col-md-6">
             <h1 class="mb-5">{{ $product->name }}</h1>
+            <h3>R$ {{ number_format($product->price, '2', ',', '.') }}</h3>
             <p class="">{{ $product->description }}</p>
             <p>Postado por: {{ $product->user->firstName }}</p>
             <button class="btn btn-warning btn-large"><i class="fa-solid fa-cart-shopping"></i> Comprar </button>
         </div>
         <div class="col-sm-6 col-md-1">
             <h5>Categoria</h5>
-            <a href="#" class="btn btn-primary mt-3">{{ $product->category->name }}</a>
+            <a href="{{route('categories.show', $product->category)}}" class="btn btn-primary mt-3">{{ $product->category->name }}</a>
         </div>
     </div>
 
