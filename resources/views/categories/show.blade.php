b37ee849a9ac513c0edf98c56be37762b639cad7
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-09 08:52:44 -0400

    create view show

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.layouts')
+
+@section('title', $category->name)
+
+@section('content')
+    <h3>Categoria: {{ $category->name }}</h3>
+    <div class="row">
+        @foreach ($products as $product)
+            <div class="col-sm-12 col-md-3">
+                <div class="card">
+                    <img src="{{ $product->url_image }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ Str::limit($product->name, 25) }}</h5>
+                        <p class="card-text">{{ Str::limit($product->description, 40) }}</p>
+                        <a href="{{ route('products.show', $product) }}" class="btn btn-primary"
+                            style="border-radius: 60vh"><i class="fa-solid fa-eye"></i></a>
+                    </div>
+                </div>
+                <br>
+            </div>
+        @endforeach
+    </div>
+    <div class="d-flex">
+        {{ $products->links() }}
+    </div>
+
+@endsection
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.layouts')
+
+@section('title', $product->name)
+
+@section('content')
+
+    <div class="row">
+        <div class="col-sm-12 col-md-5">
+            <img src="{{ $product->url_image }}" class="img-fluid" alt="...">
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <h1 class="mb-5">{{ $product->name }}</h1>
+            <p class="">{{ $product->description }}</p>
+            <p>Postado por: {{ $product->user->firstName }}</p>
+            <button class="btn btn-warning btn-large"><i class="fa-solid fa-cart-shopping"></i> Comprar </button>
+        </div>
+        <div class="col-sm-6 col-md-1">
+            <h5>Categoria</h5>
+            <a href="#" class="btn btn-primary mt-3">{{ $product->category->name }}</a>
+        </div>
+    </div>
+
+@endsection
