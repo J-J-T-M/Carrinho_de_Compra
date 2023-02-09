8133875f73d6b9d72e4f284b61289c599909dba4
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-09 08:47:47 -0400

    add route products.show

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,16 +4,15 @@
 
 @section('content')
 
-    <div class="container">
         <div class="row">
             @foreach ($products as $product)
                 <div class="col-sm-12 col-md-3">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card">
                         <img src="{{ $product->url_image }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ Str::limit( $product->name, 25) }}</h5>
                             <p class="card-text">{{ Str::limit($product->description, 40) }}</p>
-                            <a href="#" class="btn btn-primary" style="border-radius: 60vh"><i class="fa-solid fa-eye"></i></a>
+                            <a href="{{ route('products.show', $product) }}" class="btn btn-primary" style="border-radius: 60vh"><i class="fa-solid fa-eye"></i></a>
                         </div>
                     </div>
                     <br>
@@ -23,6 +22,5 @@
         <div class="d-flex">
             {{ $products->links() }}
         </div>
-    </div>
 
 @endsection
