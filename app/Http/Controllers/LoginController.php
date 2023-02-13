811916e8bc81bdfd1fc20bcf215fd1d714354ce9
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-13 16:47:22 -0400

    login controller with method auth()

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function auth(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8' , 'max:102']
+        ]);
+        if (Auth::attempt($credentials))
+            {
+                // gerar um novo id para sessão
+                $request->session()->regenerate();
+
+                // intended verifica se o user feio de algum lugar
+                return redirect()->intended('dashboard');
+            }
+        else
+        {
+            return redirect()->back()->with('erro', 'Email ou senha inválida.');
+        }
+    }
+}
