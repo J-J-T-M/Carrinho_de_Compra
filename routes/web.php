e4667e57d567afc6623b584ac61aa617353a3fc8
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-15 19:26:24 -0400

    add route logout and register

--- routes/web.php
+++ routes/web.php
@@ -5,32 +5,30 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// rotas de produtos
 Route::redirect('/', 'products/');
-
 Route::resource('products', ProductController::class);
+Route::resource('users', UserController::class);
 
+// rotas de categorias
 Route::resource('categories', CategoryController::class);
 
-// rota para o carrinho de compra
+// rotas do carrinho de compra
 Route::get('/cart', [CartController::class, 'cartList'])->name('cart');
 Route::post('/cart', [CartController::class, 'addCart'])->name('addCart');
 Route::post('/cart/remove', [CartController::class, 'cartRemove'])->name('removeCart');
 Route::post('/cart/update', [CartController::class, 'cartUpdate'])->name('updateCart');
 Route::get('/cart/clear', [CartController::class, 'cartClear'])->name('clearCart');
+
+// rotas de login
 Route::view('/login', 'login.form')->name('login.form');
 Route::post('/auth', [LoginController::class, 'auth'])->name('login.auth');
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
+Route::get('/register', [LoginController::class, 'create'])->name('login.create');
+
+// rotas do dashboard
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('admin.dashboard');
 
