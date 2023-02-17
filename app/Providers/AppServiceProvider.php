b7e2014a9db51d17557825b299f289f457a3ee40
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-17 09:19:20 -0400

    add gate to see-product

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate ;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,5 +32,9 @@ class AppServiceProvider extends ServiceProvider
 
         $categoryMenu = Category::all();
         view()->share('categoryMenu' , $categoryMenu);
+
+        Gate::define('see-product', function (User $user, Product $product){
+            return $user->id === $product->id_user;
+        });
     }
 }
