32a6df8d413ed5f9ea5e545101b65ebae8f2ed01
J-J-T-M <jonatham.junior.18@gmail.com>
2023-02-22 14:27:50 -0400

    working with the dashboard chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $user = User::all()->count();
+
+        // gráfico 1
+        $usersData = User::select([
+            DB::raw('YEAR(created_at) as year'),
+            DB::raw('COUNT(*) as total')
+        ])
+            ->groupBy('year')
+            ->orderBy('year', 'asc')
+            ->get();
+
+        // preparar arrays
+        foreach ($usersData as $userData) {
+            $year[] = $userData->year;
+            $total[] = $userData->total;
+        }
+
+        // formatar para o chart js
+        $userLabel = "'Comparativo de cadastros de usuários'";
+        $userYear = implode(', ', $year);
+        $userTotal = implode(', ', $total);
+
+        // gráfico 2
+
+        $categoriesData = Category::all();
+
+        foreach ($categoriesData as $category) {
+            $catName[] = "'" . $category->name . "'";
+            $catTotal[] = Product::where('id_category', $category->id)->count();
+        }
+        // formatar para o chart js
+        $categoryLabel = "'Comparativo entre categorias'";
+        $categoryName = implode(', ', $catName);
+        $categoryTotal = implode(',', $catTotal);
+
+        return view('admin.dashboard', [
+            'user' => $user,
+            'userLabel' => $userLabel,
+            'userYear' => $userYear,
+            'userTotal' => $userTotal,
+            'categoryLabel' => $categoryLabel,
+            'name' => $categoryName,
+            'categoryTotal' => $categoryTotal
+        ]);
     }
 }
